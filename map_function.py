a59b99ab14f2ffd8751e48d6b54feb1f0b5c8d31
May Thazin Htun <maythazinhtunn@gmail.com>
2021-12-21 14:19:01 +0630

    text adding

--- map_function.py
+++ map_function.py
@@ -2,7 +2,7 @@ nums=[1,2,3,4,5,6,7,8,9,10];
 def mapFunction(num):
     return num*2;
 
-print("Map function", map(mapFunction,nums));
+print("Map function return map object ->", map(mapFunction,nums));
 
 nums=list(map(mapFunction,nums));
 print(nums);
